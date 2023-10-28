d7b053174d6ea1c763a9ba592d542c7a69a4ee84
romanstebeletskyy1 <romanstebeletskyy1@gmail.com>
2023-10-28 21:55:33 +0300

    yep

--- src/main/java/jon/com/ua/client/YourSolver.java
+++ src/main/java/jon/com/ua/client/YourSolver.java
@@ -6,10 +6,7 @@ import com.codenjoy.dojo.services.Direction;
 import com.codenjoy.dojo.services.Point;
 import jon.com.ua.view.View;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.PriorityQueue;
+import java.util.*;
 
 /**
  * User: Ваше ім'я
@@ -32,27 +29,219 @@ public class YourSolver implements Solver<Board> {
         return getWay(board);
     }
 
+
+
     public String getWay(Board board) {
-        computePaths(createMatrix(board)[board.getHead().getX()][board.getHead().getY()]);
-        Point p = getShortestPathTo(createMatrix(board)[board.getApples().get(0).getX()]
-                [board.getApples().get(0).getY()]).get(0).point;
-
-        double y = p.getY();
-        double x = p.getX();
-
         String direction = Direction.UP.toString();
+        Vertex[][] verticesMatrix = createMatrix(board);
+        Vertex start = verticesMatrix[board.getHead().getX()][board.getHead().getY()];
+        Vertex end = verticesMatrix[board.getApples().get(0).getX()][board.getApples().get(0).getY()];
 
-        if (board.getHead().getX() == x && board.getHead().getY() < y) {
-            direction = Direction.UP.toString();
-        } else if (board.getHead().getX() > x) {
-            direction = Direction.LEFT.toString();
-        } else if (board.getHead().getX() < x) {
-            direction = Direction.RIGHT.toString();
-        } else if (board.getHead().getY() > y) {
-            direction = Direction.DOWN.toString();
+        computePaths(start);
+
+        List<Vertex> shortestPath = getShortestPathTo(end);
+
+        if (shortestPath.size() > 1) {
+            Point nextPoint = shortestPath.get(1).point;
+
+            double y = nextPoint.getY();
+            double x = nextPoint.getX();
+
+            if (board.getHead().getY() < y) {
+                direction = Direction.UP.toString();
+            } else if (board.getHead().getX() > x) {
+                direction = Direction.LEFT.toString();
+            } else if (board.getHead().getX() < x) {
+                direction = Direction.RIGHT.toString();
+            } else if (board.getHead().getY() > y) {
+                direction = Direction.DOWN.toString();
+            }
         }
+
+        if (shortestPath.size() < 2) {
+            Vertex[][] verticesMatrixWithStone = createMatrixWithStone(board);
+
+            start = verticesMatrixWithStone[board.getHead().getX()][board.getHead().getY()];
+            end = verticesMatrixWithStone[board.getApples().get(0).getX()][board.getApples().get(0).getY()];
+
+            computePaths(start);
+
+            List<Vertex> shortestPathWithStone = getShortestPathTo(end);
+
+            if (board.getSnake().size() >10 && shortestPathWithStone.size() > 1) {
+                Point nextPoint = shortestPathWithStone.get(1).point;
+                double y = nextPoint.getY();
+                double x = nextPoint.getX();
+
+                if (board.getHead().getY() < y) {
+                    direction = Direction.UP.toString();
+                } else if (board.getHead().getX() > x) {
+                    direction = Direction.LEFT.toString();
+                } else if (board.getHead().getX() < x) {
+                    direction = Direction.RIGHT.toString();
+                } else if (board.getHead().getY() > y) {
+                    direction = Direction.DOWN.toString();
+                }
+            } else {
+                List<String> availableDirections = new ArrayList<>();
+                int headX = board.getHead().getX();
+                int headY = board.getHead().getY();
+
+                if (isCollision(headX, headY + 1, board)&& isCollision(headX , headY+ 2, board)
+                        && isCollision(headX +1, headY+ 1, board)
+                        && isCollision(headX +1, headY+2, board)) {
+                    availableDirections.add(Direction.UP.toString());
+                }
+
+                if (isCollision(headX, headY - 1, board) && isCollision(headX, headY-2, board)
+                        && isCollision(headX -1, headY-1, board)
+                        && isCollision(headX - 1, headY-2, board)) {
+                    availableDirections.add(Direction.DOWN.toString());
+                }
+
+                if (isCollision(headX - 1, headY, board)&& isCollision(headX - 2, headY, board)
+                        && isCollision(headX - 1, headY-1, board)
+                        && isCollision(headX - 2, headY-1, board)) {
+                    availableDirections.add(Direction.LEFT.toString());
+                }
+
+                if (isCollision(headX + 1, headY, board) && isCollision(headX + 2, headY, board)
+                        && isCollision(headX + 2 , headY+1, board)
+                        && isCollision(headX + 1 , headY+1, board)) {
+                    availableDirections.add(Direction.RIGHT.toString());
+                }
+
+
+                if (isCollision(headX, headY + 1, board)&& isCollision(headX , headY+ 2, board)
+                        && isCollision(headX -1, headY+ 1, board)
+                        && isCollision(headX -1, headY+2, board)) {
+                    availableDirections.add(Direction.UP.toString());
+                }
+
+                if (isCollision(headX, headY - 1, board) && isCollision(headX, headY-2, board)
+                        && isCollision(headX +1, headY-1, board)
+                        && isCollision(headX + 1, headY-2, board)) {
+                    availableDirections.add(Direction.DOWN.toString());
+                }
+
+                if (isCollision(headX - 1, headY, board)&& isCollision(headX - 2, headY, board)
+                        && isCollision(headX - 1, headY+1, board)
+                        && isCollision(headX - 2, headY+1, board)) {
+                    availableDirections.add(Direction.LEFT.toString());
+                }
+
+                if (isCollision(headX + 1, headY, board) && isCollision(headX + 2, headY, board)
+                        && isCollision(headX + 2 , headY-1, board)
+                        && isCollision(headX + 1 , headY-1, board)) {
+                    availableDirections.add(Direction.RIGHT.toString());
+                }
+
+
+
+                if (isCollision(headX, headY + 1, board)&& isCollision(headX , headY+ 2, board)
+                        && isCollision(headX , headY+ 3, board)) {
+                    availableDirections.add(Direction.UP.toString());
+                }
+
+                if (isCollision(headX, headY - 1, board) && isCollision(headX, headY-2, board)
+                        && isCollision(headX, headY-3, board)) {
+                    availableDirections.add(Direction.DOWN.toString());
+                }
+
+                if (isCollision(headX - 1, headY, board)&& isCollision(headX - 2, headY, board)
+                        && isCollision(headX - 3, headY, board)) {
+                    availableDirections.add(Direction.LEFT.toString());
+                }
+
+                if (isCollision(headX + 1, headY, board) && isCollision(headX + 2, headY, board)
+                        && isCollision(headX + 3 , headY, board)) {
+                    availableDirections.add(Direction.RIGHT.toString());
+                }
+
+
+
+
+                if (isCollision(headX, headY + 1, board)&& isCollision(headX , headY+ 2, board)
+                        && isCollision(headX +1, headY+ 2, board)) {
+                    availableDirections.add(Direction.UP.toString());
+                }
+
+                if (isCollision(headX, headY - 1, board) && isCollision(headX, headY-2, board)
+                        && isCollision(headX-1, headY-2, board)) {
+                    availableDirections.add(Direction.DOWN.toString());
+                }
+
+                if (isCollision(headX - 1, headY, board)&& isCollision(headX - 2, headY, board)
+                        && isCollision(headX -2 , headY-1, board)) {
+                    availableDirections.add(Direction.LEFT.toString());
+                }
+
+                if (isCollision(headX + 1, headY, board) && isCollision(headX + 2, headY, board)
+                        && isCollision(headX +2 , headY+1, board)) {
+                    availableDirections.add(Direction.RIGHT.toString());
+                }
+
+
+
+
+                if (isCollision(headX, headY + 1, board)&& isCollision(headX , headY+ 2, board)) {
+                    availableDirections.add(Direction.UP.toString());
+                }
+
+                if (isCollision(headX, headY - 1, board) && isCollision(headX, headY-2, board)) {
+                    availableDirections.add(Direction.DOWN.toString());
+                }
+
+                if (isCollision(headX - 1, headY, board)&& isCollision(headX - 2, headY, board)) {
+                    availableDirections.add(Direction.LEFT.toString());
+                }
+
+                if (isCollision(headX + 1, headY, board) && isCollision(headX + 2, headY, board) ) {
+                    availableDirections.add(Direction.RIGHT.toString());
+                }
+
+
+
+                if (isCollision(headX, headY + 1, board)) {
+                    availableDirections.add(Direction.UP.toString());
+                }
+
+                if (isCollision(headX, headY - 1, board) ) {
+                    availableDirections.add(Direction.DOWN.toString());
+                }
+
+                if (isCollision(headX - 1, headY, board)) {
+                    availableDirections.add(Direction.LEFT.toString());
+                }
+
+                if (isCollision(headX + 1, headY, board) ) {
+                    availableDirections.add(Direction.RIGHT.toString());
+                }
+
+                if (!availableDirections.isEmpty()) {
+                    return availableDirections.get(0);
+                }
+            }
+        }
+
         return direction;
     }
+
+        private boolean isCollision(int x, int y, Board board) {
+            List<Point> badPoints = new ArrayList<>();
+            badPoints.addAll(board.getSnake());
+            badPoints.addAll(board.getStones());
+            badPoints.addAll(board.getWalls());
+
+            for (Point point : badPoints) {
+                if (point.getX() == x && point.getY() == y) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
 
     public static void computePaths(Vertex source) {
         source.minDistance = 0;
@@ -110,6 +299,45 @@ public class YourSolver implements Solver<Board> {
         return verticesMatrix;
     }
 
+    public Vertex[][] createMatrixWithStone(Board board) {
+        List<Point> emptyPoints = board.get(Elements.NONE);
+        emptyPoints.add(board.getHead());
+        emptyPoints.add(board.getApples().get(0));
+        emptyPoints.add(board.getStones().get(0));
+        Vertex[][] verticesMatrix = new Vertex[board.size()][board.size()];
+
+        for (Point point : emptyPoints) {
+            verticesMatrix[point.getX()][point.getY()] = new Vertex(point);
+        }
+
+        for (int i = 0; i < verticesMatrix.length - 1; i++) {
+            for (int j = 0; j < verticesMatrix.length - 1; j++) {
+                if (verticesMatrix[i][j] == null) {
+                    continue;
+                }
+
+                if (verticesMatrix[i][j - 1] != null) {
+                    verticesMatrix[i][j].edges.add(new Edge(verticesMatrix[i][j - 1], 1));
+                }
+
+                if (verticesMatrix[i][j + 1] != null) {
+                    verticesMatrix[i][j].edges.add(new Edge(verticesMatrix[i][j + 1], 1));
+                }
+
+
+                if (verticesMatrix[i - 1][j] != null) {
+                    verticesMatrix[i][j].edges.add(new Edge(verticesMatrix[i - 1][j], 1));
+                }
+
+
+                if (verticesMatrix[i + 1][j] != null) {
+                    verticesMatrix[i][j].edges.add(new Edge(verticesMatrix[i + 1][j], 1));
+                }
+            }
+        }
+
+        return verticesMatrix;
+    }
     public List<Point> getPath() {
         return path;
     }
